ed67757cbcb8b18a3485661214b43e3773dbf0fc
bishalkunwar <bishal.kunwar487@gmail.com>
2021-12-19 16:21:20 +0545

    writing hello + variableName

---
+++ Desktop/Desktop_Lite/reactApp/learning/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        Hello World welcome to react learning. <
+        /header> < /
+        div >
+    );
+}
+
+export default App;
