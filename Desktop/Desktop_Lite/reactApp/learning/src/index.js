e85494f21d0115d9acdc7efd5ffdec83425bcec8
bishalkunwar <bishal.kunwar487@gmail.com>
2021-12-23 20:56:25 +0545

    form input and state lifting example done

--- Desktop/Desktop_Lite/reactApp/learning/src/index.js
+++ Desktop/Desktop_Lite/reactApp/learning/src/index.js
@@ -58,48 +58,138 @@ import ReactDOM from "react-dom";
 //     />, document.getElementById('root')
 // );
 
-import { render } from "ejs";
 
 
-class NameForm extends React.Component {
+// import { render } from "ejs";
+
+
+// class NameForm extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { value: '' };
+
+//         this.handleChange = this.handleChange.bind(this);
+//         this.handleSubmit = this.handleSubmit.bind(this);
+
+//     }
+
+//     handleChange(event) {
+//         this.setState({ value: event.target.value });
+//     };
+
+//     handleSubmit(event) {
+//         alert('A name is submitted: ' + this.state.value);
+//         event.preventDefault();
+//     };
+
+
+
+//     render() {
+//         return ( <
+//             form onSubmit = { this.handleSubmit } >
+//             <
+//             label >
+//             Name: < input type = "text"
+//             value = { this.state.value }
+//             onChange = { this.handleChange } > < /input> < /
+//             label > <
+//             input type = "submit"
+//             value = "submit" > < /input> < /
+//             form >
+//         );
+//     };
+
+
+// };
+
+// ReactDOM.render( <
+//     NameForm / > , document.getElementById('root')
+// );
+
+
+//Example of lifting state at real time with the example of temprature and condition to boil water check
+
+
+// --------------------------------------------------
+
+function BoilingVerdict(props) {
+    if (props.celsius >= 100) {
+        return <p > The water would boil. < /p>;
+    }
+    return <p > The water would not boil. < /p>;
+};
+
+
+class Calculator extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { value: '' };
-
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-
+        this.state = { temperature: '' };
     }
 
-    handleChange(event) {
-        this.setState({ value: event.target.value });
+    handleChange(e) {
+        this.setState({ temperature: e.target.value });
     };
-
-    handleSubmit(event) {
-        alert('A name is submitted: ' + this.state.value);
-        event.preventDefault();
-    };
-
-
 
     render() {
+        const temperature = this.state.temperature;
+
         return ( <
-            form onSubmit = { this.handleSubmit } >
+            fieldset >
             <
-            label >
-            Name: < input type = "text"
-            value = { this.state.value }
-            onChange = { this.handleChange } > < /input> < /
-            label > <
-            input type = "submit"
-            value = "submit" > < /input> < /
-            form >
+            legend > Enter temperature in Celsius: < /legend> <
+            input value = { temperature }
+            onChange = { this.handleChange }
+            /> <
+            BoilingVerdict celsius = { parseFloat(temperature) }
+            /> < /
+            fieldset >
         );
     };
 
-
 };
 
+
 ReactDOM.render( <
-    NameForm / > , document.getElementById('root')
+    Calculator / > , document.getElementById('root')
 );
+
+
+// function BoilingVerdict(props) {
+//     if (props.celsius >= 100) {
+//         return <p > The water would boil. < /p>;
+//     }
+//     return <p > The water would not boil. < /p>;
+// }
+
+// class Calculator extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.handleChange = this.handleChange.bind(this);
+//         this.state = { temperature: '' };
+//     }
+
+//     handleChange(e) {
+//         this.setState({ temperature: e.target.value });
+//     }
+
+//     render() {
+//         const temperature = this.state.temperature;
+//         return ( <
+//             fieldset >
+//             <
+//             legend > Enter temperature in Celsius: < /legend> <
+//             input value = { temperature }
+//             onChange = { this.handleChange }
+//             /> <
+//             BoilingVerdict celsius = { parseFloat(temperature) }
+//             /> < /
+//             fieldset >
+//         );
+//     }
+// }
+
+// ReactDOM.render( <
+//     Calculator / > ,
+//     document.getElementById('root')
+// );
